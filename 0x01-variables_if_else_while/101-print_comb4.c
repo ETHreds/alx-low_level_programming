f77a8ad6d696b659e9bf93405a1163bc006daf63
ETHreds <jcmaglaw@gmail.com>
2022-06-05 20:50:42 +0000

    bety style formmat fixed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,6 +9,7 @@ int main(void)
 	int i = 0;
 	int j = 0;
 	int k = 0;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
@@ -20,11 +21,11 @@ int main(void)
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-				if (!(i == 7 && j == 8 && k == 9))
+					if (!(i == 7 && j == 8 && k == 9))
 					{
 						putchar(',');
 						putchar(' ');
-					}
+						}
 				}
 			}
 		}

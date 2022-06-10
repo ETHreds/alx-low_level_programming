7177c25b53286c63fa511827c56ad281aba5a7ee
ETHreds <jcmaglaw@gmail.com>
2022-06-10 01:29:20 +0000

    Source code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - get product of to digit
+ * @a: takes  integer
+ * @b: takes integer
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+
+{
+	int product;
+
+	produt = a * b;
+
+	return (product);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight lines on stdout
+ * @n: the length of the line
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}

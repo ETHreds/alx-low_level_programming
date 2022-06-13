54ebf3df0729d134ec1d309eed9da0b011e066d7
ETHreds <jcmaglaw@gmail.com>
2022-06-13 23:44:03 +0000

    initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main."
+
+/**
+ * reset_to_98 -takes a pointer as parameter and points it to 98
+ * @n: pointer is an integer
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+		*n = 98;
+}

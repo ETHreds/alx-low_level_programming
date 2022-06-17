e7f31920c51848cfd84a643b3e8619a66e7497ec
ETHreds <jcmaglaw@gmail.com>
2022-06-17 04:50:45 +0300

    maybe my last commit with alx

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,21 +3,18 @@
 /**
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
- *
  * Return: n
  */
-
-char *string_toupper(char *str)
-
+char *string_toupper(char *n)
 {
+	int i;
 
-	int i = 0;
-
-	while (*(s + i))
+	i = 0;
+	while (n[i] != '\0')
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-			*(s + i) -= 'a' - 'A';
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
 		i++;
 	}
-	return (s);
+	return (n);
 }

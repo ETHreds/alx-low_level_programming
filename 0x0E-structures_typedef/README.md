ee2b4c155b2a27201f7f9f5fdd87857d402564b4
ETHreds <jcmaglaw@gmail.com>
2022-07-04 20:35:07 +0000

    initial commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,10 @@
+dog.h -- header file wiith a new type struct dog
+
+1-init_dog.c -- function to initialise the struct dog 
+
+2-print_dog.c-- functon that prints struct dog
+
+4-new_dog/c--- creates a new dog
+
+5-free_dog--  frees dog
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,34 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ *struct dog- dog information
+ *@name: dog's nmae
+ *@age: dog's age
+ *@owner: dogs owner name
+ *
+ *Description: just a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+/**
+ *dog_t- typedrf for struct dog
+ */
+
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif

b50291b836db06334e4945857c919bebac17c224
ETHreds <jcmaglaw@gmail.com>
2022-07-04 22:04:31 +0000

    struct dog initialiser

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,19 @@
-Testing th eauto push stuff
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *init_dog-initializes struct dog
+ *@d:pointer to struct dog
+ *@name:dogs name
+ *@age: dogs age
+ *@owner: owner of th edog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

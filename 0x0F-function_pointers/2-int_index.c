8da70d6885f109d829743ce8d878d3765443c521
ETHreds <jcmaglaw@gmail.com>
2022-07-07 00:42:57 +0000

    number 2 compiles perfectly

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ *int_index- searches integer
+ *@array:array to be canned
+ *@size: size of the array
+ *@comp: compaator function pointer
+ *
+ *Return: index of first similar element(on succes)
+ *	0 on no matches
+ *	-1 when size is negative
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp && array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+
+	return (-1);
+}

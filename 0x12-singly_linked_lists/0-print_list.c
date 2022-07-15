f753e11d1e9d74a64a85291e4944e3b3788144b8
ETHreds <jcmaglaw@gmail.com>
2022-07-15 00:57:59 +0000

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *print_list-prints all elements in list
+ *@h:pointer to th elist
+ *
+ *Return:the number of nodes printed
+ */
+
+size_t print_list(const list_t *h);
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			print("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		s++;
+	}
+
+	return (s);
+}

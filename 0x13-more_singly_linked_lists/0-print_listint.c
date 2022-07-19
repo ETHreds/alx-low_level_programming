097b27065d69aa7a96a7d5bc4f5894a829a3e34c
ETHreds <jcmaglaw@gmail.com>
2022-07-19 18:25:25 +0000

    revision

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,18 +3,18 @@
 /**
 *print_listint - prints all the elements of a list.
 *@h: head of a list.
-*Return: numbers of nodes.
+*Return: number of nodes.
 */
 
 size_t print_listint(const listint_t *h)
 {
-	size_t s = 0;
+	unsigned int count = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		s++;
+		count++;
 	}
-	return (s);
+	return (count);
 }
